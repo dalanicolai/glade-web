6b144e41e792697a2642f54c831aeab1c9fe66a0
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2018-01-09 13:47:17 -0300

    Add news about new IRC channel

--- newsitems.py
+++ newsitems.py
@@ -15,6 +15,11 @@ item = lambda *x: items.append(x)
 
 # http://news.gmane.org/gmane.comp.gnome.gtk%2B.python/
 
+item("""Glade 3.21.0 released""", (2018, 1, 8),
+     """
+    <p>New IRC channel!!!</p>
+    <p>Glade IRC channel moved to #glade</p>""", 'Juan Pablo Ugarte')
+
 item("""Glade 3.21.0 released""", (2017, 12, 30),
      """
     <p>Glade 3.21.0 is now available for download.</p>
